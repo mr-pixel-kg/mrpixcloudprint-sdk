f9516762cf88da161fdd39f18fed393edd7d3a25
Manuel Kienlein <manuel.kienlein@gmx.de>
2022-03-11 17:51:59 +0100

    Added license

--- tests/CloudPrintTest.php
+++ tests/CloudPrintTest.php
@@ -4,6 +4,9 @@ namespace Mrpix\CloudPrintSDK\Tests;
 
 class CloudPrintTest
 {
-    const USER_EMAIL = 'email@example.com';
-    const USER_PASSWORD = 'password';
+    //const USER_EMAIL = 'email@example.com';
+    //const USER_PASSWORD = 'password';
+
+    const USER_EMAIL = 'manuel.kienlein@mr-pixel.de';
+    const USER_PASSWORD = 'MrpixCloudPrint_admin';
 }
